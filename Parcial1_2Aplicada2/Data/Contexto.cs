7ad43d0cc354c1eac78cc92a64c8706c98622762
Emmanuel Suarez Taveras <manuel.413@hotmail.com>
2020-02-19 15:50:56 -0400

    Agregando el proyecto

---
+++ Parcial1_2Aplicada2/Data/Contexto.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parcial1_2Aplicada2.Data
+{
+    public class Contexto : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source=DataBase/Data.db");
+        }
+    }
+}
